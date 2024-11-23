443de7bc7f8dc8f7f28ab6d061ee96aaceb65e0b
MaikVuolo <maikvb88@gmail.com>
2024-11-23 18:55:54 -0300

    feat: :sparkles: add: flag de acesso e testes em /eventos

--- src/controllers/eventosController.js
+++ src/controllers/eventosController.js
@@ -2,12 +2,18 @@
 import Evento from '../models/evento.js';
 
 class EventosController {
+  static liberaAcessoEventos = () => process.env.EVENTO_FLAG === 'true';
+
   static listarEventos = async (req, res) => {
-    try {
-      const eventos = await Evento.pegarEventos();
-      res.status(200).json(eventos);
-    } catch (err) {
-      res.status(500).json(err.message);
+    if (this.liberaAcessoEventos()) {
+      try {
+        const eventos = await Evento.pegarEventos();
+        res.status(200).json(eventos);
+      } catch (err) {
+        res.status(500).json(err.message);
+      }
+    } else {
+      res.status(404).send();
     }
   };
 }
